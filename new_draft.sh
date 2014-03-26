5515732e9549e03e247059dbb335b44445cde565
Erik Bjäreholt <erik.bjareholt@gmail.com>
2014-03-26 11:19:08 +0100

    Added new_draft script

---
+++ new_draft.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Enter the title of the post: "
+read title
+
+echo "Enter the author of the post ['Erik Bj&auml;reholt']: "
+read author
+if [ -z "$author" ] 
+then
+    author="Erik Bj&auml;reholt"
+fi
+
+echo "---
+layout: post
+title: \"$title\"
+author: \"$author\"
+date: $(date +'%Y/%m/%d %H:%M')
+status: In Progress
+categories: $categories
+---" > "_drafts/$(date +'%Y-%m-%d')-${title// /-}.md"
